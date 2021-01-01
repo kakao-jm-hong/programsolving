9514f010c6d341298ed8360128b694c9ce15be2a
redbin <wjdals0471@naver.com>
2021-01-01 21:32:58 +0900

    멀쩡한사각형

--- JavaScript/멀쩡한사각형.js
+++ JavaScript/멀쩡한사각형.js
@@ -1,4 +1,22 @@
 function solution(w, h) {
-    var answer = 1;
-    return answer;
+    return w * h - (w + h - GCD(w, h));
 }
+
+function GCD(a, b) {
+    let tmp, n;
+
+    if (a < b) {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    while (b != 0) {
+        n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+solution(8, 12);
